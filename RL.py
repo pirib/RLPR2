e8e909553159dc121dd568249fa9c3ad8eaa281f
Piri <212310@usn.no>
2021-04-19 09:54:42 +0200

    Clean up

--- RL.py
+++ RL.py
@@ -23,6 +23,11 @@ class RL():
         # 3. Initialize the Neural Network (adding the input layer which is the square of the board size )
         self.ANET = an.ANET( [board_size**2] + nn_layers + [board_size**2] , nn_optimizer, save_path)
         
+        # Alternatively load the ANET here
+        self.ANET = an.ANET()
+        self.ANET.load(episode, "foldername")
+        
+        
         # 4. Start working through episodes/epochs
         for e in range(episodes):
             
